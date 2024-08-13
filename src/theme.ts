fcd33fc52b3d2b14455187d684cdd4eb98c46c7a
小帅 <2502797161@qq.com>
2024-08-13 19:05:22 +0800

    implement dark mode

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+// 用主题配置ThemeConfig来注释config，这样就能访问它的属性，比如初始颜色等
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
+//注意：config: ThemeConfig的写法解释如下；'config' 是一个变量名，' : ' 后面的 ThemeConfig 是类型注解，指定 config 变量的类型。
+//这段代码的结果：声明一个名为 config 的常量，指定 config 的类型为 ThemeConfig，给 config 赋值一个对象，这个对象有一个属性 initialColorMode，其值为字符串 "dark"。
+
+const theme = extendTheme({ config });
+export default theme;
