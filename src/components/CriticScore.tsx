d87a68b0dcec21463eb23c0e8f3a7c61adfb7779
大帅 <13631647579@163.com>
2024-08-19 14:38:01 +0800

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  //分数超过75分就显示为绿色，超过60分显示为黄色，低于60分的默认颜色
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    //colorScheme是chakra库封装的属性，
+    //如果直接设置color属性，color属性只能设置前景颜色，但是colorScheme可以设置前景色和背景色，包括边框阴影色等；
+    <Badge fontSize="14px" paddingX={2} borderRadius="4px" colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -13,11 +14,13 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-
-        {/* 遍历game.parent_platform（是数组对象），将每个对象作为参数p，将对象里的属性platform对应的对象值，作为结果返出去。（因为每个game对象数据里的parent_platform，结构是对象数组，会有多个对象，每条对象数据里，又有一个属性,名为platform，platform值是包含id等数据的对象；） */}
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          {/* 遍历game.parent_platform（是数组对象），将每个对象作为参数p，将对象里的属性platform对应的对象值，作为结果返出去。（因为每个game对象数据里的parent_platform，结构是对象数组，会有多个对象，每条对象数据里，又有一个属性,名为platform，platform值是包含id等数据的对象；） */}
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
