2e67d3c33bb6429c8af976ce32ad16c663263c98
大帅 <13631647579@163.com>
2024-08-19 20:54:07 +0800

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ const GameCard = ({ game }: Props) => {
   return (
     // 如果只设置borderRadius效果，只能看到图片下边框是圆角，上边框依然是方角，原因就是图片大于容器，所以我们看不到上边框圆角；
     // 解决办法：设置溢出隐藏
-    <Card borderRadius={10} overflow="hidden">
+    <Card borderRadius={10} overflow="hidden" width="300px">
       {/* <Image src={game.background_image} /> */}
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,22 +23,30 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setErrors] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     // 浏览器终止请求
     const controller = new AbortController();
+    setLoading(true);
     apiClients
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setErrors(err.message);
+        setLoading(false);
       });
+    // .finally(() => setLoading(false)); //因为react严格模式下finally里设置这个不生效，所以写在了res和err里面
+
     // 清理函数，终止数据请求，在每次组件重新渲染时都会被调用（在下一次 effect 运行之前）。
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
