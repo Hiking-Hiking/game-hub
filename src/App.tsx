d898eed468a3c3aaaca77b522620c376123ecc7c
大帅 <13631647579@163.com>
2024-08-20 18:37:27 +0800

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,13 @@ function App() {
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`, //宽于1024px的大屏幕
         }}
+        templateColumns={{
+          // '1fr'，意味着一个分数，即 列会拉伸从而占据所有空间；
+          // 在基础设备（即小型设备）上，只显示的这一列占据所有可用空间；
+          base: "1fr",
+          // 在大设备上，第一列（即左侧的侧边栏宽度为固定的200px），第二列（即右侧的主体游戏网格部分）占据剩余所有空间；
+          lg: "200px 1fr",
+        }}
       >
         <GridItem area="nav">
           <NavBar />
@@ -18,7 +25,7 @@ function App() {
 
         {/* 只在大于1024px的宽屏上显示aside部分 */}
         <Show above="lg">
-          <GridItem area="aside">
+          <GridItem area="aside" paddingX={5}>
             <GenreList />
           </GridItem>
         </Show>
--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 const GameCardContainer = ({ children }: Props) => {
   return (
     //<Box>标签是chakra库的一级标签，显示在页面上就是<div>
-    <Box borderRadius={10} overflow="hidden" width="300px">
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
