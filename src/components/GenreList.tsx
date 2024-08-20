bd43e5a1923bd98d5a1af9bcdd12924709e81f93
大帅 <13631647579@163.com>
2024-08-20 18:52:15 +0800

    Show a spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/getCroppedImageUrl";
 
@@ -6,6 +6,10 @@ const GenreList = () => {
   const { data, error, isLoading } = useGenres();
   return (
     <List>
+      {/* 获取数据时显示加载效果 */}
+      {isLoading && <Spinner />}
+      {/* 因为不想在页面的不同部分,出现多个不同的错误提示，所以如果侧边栏获取数据错误，只是返回空值，什么都不渲染。 */}
+      if(error)return null;
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
