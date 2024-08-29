9eb2534d7727563a2fdb1fa70677a5eaa23b04c2
大帅 <13631647579@163.com>
2024-08-29 17:01:39 +0800

    ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
-import useData from "./useData";
+// import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -8,6 +9,9 @@ export interface Genre {
   image_background: string;
 }
 //不要忘了给useData传入接口类型<Genre>，否则在GenreList组件渲染数据时，因为不知道数据类型会报错；
-const useGenres = () => useData<Genre>("/genres");
+// const useGenres = () => useData<Genre>("/genres");
+
+// 另一种方案：使用静态数据
+const useGenres = () => ({ data: genres, error: null, isLoading: false });
 
 export default useGenres;
